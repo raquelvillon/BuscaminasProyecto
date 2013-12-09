cc46a4185c0a1f523979f4dfc9bf095be83b8824
Raquel Villon <raqevill@espol.edu.ec>
2013-12-09 09:24:10 -0500

    seekbars en personalizado. funciona seekbar de ancho

--- FlujoActivities/src/com/example/flujoactivities/Personalizado.java
+++ FlujoActivities/src/com/example/flujoactivities/Personalizado.java
@@ -3,15 +3,48 @@ package com.example.flujoactivities;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Personalizado extends Activity {
-
+	private SeekBar seekBar;
+	 private TextView textViewSeekBar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_personalizado);
+		seekBar = (SeekBar) findViewById(R.id.seekBarAncho);
+        textViewSeekBar = (TextView) findViewById(R.id.valorAncho);
+        textViewSeekBar.setText("9");
+        
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {            
+            
+           
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                 //la Seekbar siempre empieza en cero, si queremos que el valor mínimo sea otro podemos modificarlo
+                 textViewSeekBar.setText(progress + 9 + "");                 
+            }
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+           
+     });
 	}
 
 	/*
